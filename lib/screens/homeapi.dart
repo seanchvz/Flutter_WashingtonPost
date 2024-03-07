2ac39432b485f28ec8f50e297d31c680a8b40aa0
seanchvz <seann.chvz@gmail.com>
2024-03-07 10:07:04 +0800

    March 7

---
+++ lib/screens/homeapi.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class HomeAPI extends StatefulWidget {
+  const HomeAPI({super.key});
+
+  @override
+  State<HomeAPI> createState() => _HomeAPIState();
+}
+
+class _HomeAPIState extends State<HomeAPI> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
